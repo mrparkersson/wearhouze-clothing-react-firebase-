61a3aeed89dbd81c20743af95b6042cde4885d40
mrparkersson <amoparkerprince78@gmail.com>
2022-07-06 13:29:11 +0000

    Add a temporal shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/routes/home/home.component';
 import Navigation from './components/routes/navigation/navigation.component';
+import SignIn from './components/routes/signin/sign-in.component';
+
+const Shop = () => {
+  return <h1>I am shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
