5dfe3312fb60718c2a58e40ceabd2fcf03edea81
mrparkersson <amoparkerprince78@gmail.com>
2022-07-06 13:30:19 +0000

    Add link for signin routing

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,15 +1,22 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import './navigation.styles.scss';
+import { ReactComponent as CrwnLogo } from '../../../assets/crown.svg';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>Logo</div>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
-          <Link className="nav-link" to="/"></Link>
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
         </div>
       </div>
 
