4b27903e87c6b4ad50ea54fe2982c8b2ca27cc0d
mrparkersson <amoparkerprince78@gmail.com>
2022-07-08 15:53:22 +0000

    Add new shop component and remove from app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/routes/home/home.component';
 import Navigation from './components/routes/navigation/navigation.component';
 import Authentication from './components/routes/authenticate/authenticate.component';
-
-const Shop = () => {
-  return <h1>I am shop page</h1>;
-};
+import Shop from './components/routes/shop/shop.component';
 
 const App = () => {
   return (
