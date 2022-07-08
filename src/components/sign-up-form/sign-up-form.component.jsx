b4f342ba7e95f4fd9216b75cf8164173646a68cf
mrparkersson <amoparkerprince78@gmail.com>
2022-07-08 05:25:54 +0000

    Remove console log statements

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -42,6 +42,7 @@ const SignUpForm = () => {
         password
       );
       await createUserDocumentFromAuth(user, { displayName });
+
       resetFormFields();
     } catch (error) {
       if (error.code === 'auth/email-already-in-use') {
