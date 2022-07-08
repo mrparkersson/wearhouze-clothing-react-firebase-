64ef5039ee57140cf59eac013cebbae98e4ddbe2
mrparkersson <amoparkerprince78@gmail.com>
2022-07-08 15:55:38 +0000

    create product component

---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState, useEffect } from 'react';
+import SHOP_DATA from '../shop-data.json';
+
+export const ProductContext = createContext({
+  products: [],
+});
+
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState(SHOP_DATA);
+  const value = { products };
+  return (
+    <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
+  );
+};
