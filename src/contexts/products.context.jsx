ad4b9d1a11f5e2d508cd3036df595afc4cabb0c7
mrparkersson <amoparkerprince78@gmail.com>
2022-07-08 17:38:23 +0000

    Add product component

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState } from 'react';
 import SHOP_DATA from '../shop-data.json';
 
 export const ProductContext = createContext({
