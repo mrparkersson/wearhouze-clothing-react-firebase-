df4878a43af543382bdf06da85c69277848552d3
mrparkersson <amoparkerprince78@gmail.com>
2022-07-08 17:36:24 +0000

    Add price for shopping bag icon

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({ product }) => {
       <img src={imageUrl} alt={`${name}`} />
       <div className="footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">${price}</span>
       </div>
       <Button buttonType="inverted">Add to cart</Button>
     </div>
