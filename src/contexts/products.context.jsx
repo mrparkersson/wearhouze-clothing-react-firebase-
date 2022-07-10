9aa5b0bf2751fae8badbaf264e23cf8b89adb6d0
mrparkersson <amoparkerprince78@gmail.com>
2022-07-10 16:29:03 +0000

    set shopt data to empty array to be fetched from db

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,12 +1,12 @@
 import { createContext, useState } from 'react';
-import SHOP_DATA from '../shop-data.json';
+import SHOP_DATA from '../shop-data.js';
 
 export const ProductContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(SHOP_DATA);
+  const [products, setProducts] = useState([]);
   const value = { products };
   return (
     <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
