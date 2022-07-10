e31cc9fb132e447f088368b2db65b46f7ec2eb1d
mrparkersson <amoparkerprince78@gmail.com>
2022-07-10 05:46:37 +0000

    Import checkout component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/routes/home/home.component';
 import Navigation from './components/routes/navigation/navigation.component';
 import Authentication from './components/routes/authenticate/authenticate.component';
 import Shop from './components/routes/shop/shop.component';
+import Checkout from './components/routes/checkout/checkout.component';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
