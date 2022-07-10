5455ae0c1a0e7f826093c21f5ea27443193a3d5c
mrparkersson <amoparkerprince78@gmail.com>
2022-07-10 21:11:52 +0000

    Add shop component and import statements

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,16 +1,28 @@
-import { useContext } from 'react';
-import { ProductContext } from '../../../contexts/products.context';
+import { useContext, Fragment } from 'react';
+
 import ProductCard from '../../product-card/product-card.component';
+
+import { CategoriesContext } from '../../../contexts/categories.context';
+
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { products } = useContext(ProductContext);
+  const { categoriesMap } = useContext(CategoriesContext);
+
   return (
-    <div className="products-container">
-      {products.map((product) => {
-        return <ProductCard product={product} key={product.id} />;
-      })}
-    </div>
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className="products-container">
+            {categoriesMap[title].map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
+      ))}
+    </Fragment>
   );
 };
+
 export default Shop;
